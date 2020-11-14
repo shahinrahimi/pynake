7b1ab5c5930650691e7d3126defe931272aac62f
Shahn <fulcon.ixi@gmail.com>
2020-11-14 23:03:43 +0300

    Initialize Player

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ class Cell():
         self.y = y
         self.state = 0 # 0 is empty 1 is has some food 2 is accupyied
         self.color = Colors.BACKGROUND_COLOR
-
+class Snake():
+    def __init__(self,currrnt_cell):
+        self.head = None
+        self.body = []
+        self.tail = None
 
 class Colors():
     BACKGROUND_COLOR = (0,0,0)
     RED_COLOR = (255,0,0)
-
+    GREEN_COLOR = (0,255,0)
+    BLUE_COlOR = (0,0,255)
 
 class Game():
     def __init__(self):
@@ -29,13 +34,27 @@ class Game():
 
         self.cells = []
         self.init_cells()
+        self.init_player()
+
+        self.player = []
+        self.player_direction = (0,0)
 
     def run(self):
         running = True
         while running:
+            events = pygame.event.get()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+            key_input = pygame.key.get_pressed()
+            if key_input[pygame.K_LEFT]:
+                self.player_direction = (-1,0)
+            if key_input[pygame.K_UP]:
+                self.player_direction = (0,-1)
+            if key_input[pygame.K_RIGHT]:
+                self.player_direction = (1,0)
+            if key_input[pygame.K_DOWN]:
+                self.player_direction = (0,1)
             self.render_cells()
             pygame.display.update()
         pygame.quit()
@@ -49,6 +68,11 @@ class Game():
                 cell = Cell(pos)
                 cell.color = Colors.RED_COLOR
                 self.cells.append(cell)
+
+    def init_player(self):
+        player = random.choice(self.cells)
+        player.color = Colors().BLUE_COlOR
+
 
     def render_cells(self):
         screen = self.screen
