edfa205f641e49324aee82a757a6b83e4e923939
Shahn <fulcon.ixi@gmail.com>
2020-11-16 17:15:26 +0300

    Basic Game Run Well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-import random
-
+import random,time
 class Cell():
     def __init__(self,row,column,size_width,size_height):
         self.size_width = size_width
@@ -33,41 +32,47 @@ class Colors():
 class Game():
     def __init__(self):
         pygame.init()
+        self.gamestarted = 0 # 0 not started yet 1 is started
         self.height = 800
         self.width = 800
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.screen.fill(Colors().BACKGROUND_COLOR)
-        self.cell_size_width = 10
-        self.cell_size_height = 10
+        self.cell_size_width = 25
+        self.cell_size_height = 25
         self.cell_count_x = self.width//self.cell_size_width
         self.cell_count_y = self.height//self.cell_size_height
-
+        self.time_laps = 0.3
         self.player_curdire = Direction().STOP
 
         self.cells = []
         self.init_cells()
         self.player = []
         self.init_player()
+        self.food = []
+        self.food_generate()
+
 
     def run(self):
         running = True
         while running:
+            time.sleep(self.time_laps)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.KEYUP:
+                    self.gamestarted = 1
                     if event.key == pygame.K_LEFT and self.player_curdire != Direction.RIGHT:
                         self.player_curdire = Direction.LEFT
-                        self.player_move()
-                    if event.key == pygame.K_UP and self.player_curdire != Direction.DOWN:
+                    elif event.key == pygame.K_UP and self.player_curdire != Direction.DOWN:
                         self.player_curdire = Direction.UP
-                        self.player_move()
-                    if event.key == pygame.K_RIGHT and self.player_curdire != Direction.LEFT:
+                    elif event.key == pygame.K_RIGHT and self.player_curdire != Direction.LEFT:
                         self.player_curdire = Direction.RIGHT
-                        self.player_move()
-                    if event.key == pygame.K_DOWN and self.player_curdire != Direction.UP:
+                    elif event.key == pygame.K_DOWN and self.player_curdire != Direction.UP:
                         self.player_curdire = Direction.DOWN
-                        self.player_move()
+
+
+            if self.gamestarted:
+                self.player_move()
 
             # key_input = pygame.key.get_pressed()
             #
@@ -94,23 +99,46 @@ class Game():
         self.cell_convert_to_player(head)
         self.player.append(head)
 
-    def cell_convert_to_player(self,cell):
-        cell.color = Colors().GREEN_COLOR
-        cell.state = 2
+    def food_generate(self):
+        while True:
+            food = random.choice(self.cells)
+            if food.state == 0:
+                self.cell_convert_to_food(food)
+                self.food.append(food)
+                return
+
     def cell_convert_to_blank(self,cell):
         cell.color = Colors().BACKGROUND_COLOR
         cell.state = 0
+    def cell_convert_to_food(self,cell):
+        cell.color = Colors().GREEN_COLOR
+        cell.state = 1
+    def cell_convert_to_player(self,cell):
+        cell.color = Colors().BLUE_COlOR
+        cell.state = 2
+    def cell_conver_to_looserhead(self,cell):
+        cell.color = Colors().RED_COLOR
 
     def player_move(self):
-        old_head = self.player[0]
+        tail = self.player[0]
+        old_head = self.player[-1]
         new_head = self.get_cell_by_curdir()
-        self.cell_convert_to_player(new_head)
-        self.cell_convert_to_blank(old_head)
-        self.player.remove(old_head)
-        self.player.append(new_head)
+        if new_head.state == 0:
+            self.cell_convert_to_player(new_head)
+            self.cell_convert_to_blank(tail)
+            self.player.remove(tail)
+            self.player.append(new_head)
+        elif new_head.state == 1:
+            self.cell_convert_to_player(new_head)
+            self.player.append(new_head)
+            self.food_generate()
+        elif new_head.state == 2:
+            self.gamestarted = 0
+            print("You Loos")
+
 
     def get_cell_by_curdir(self):
-        head = self.player[0]
+        head = self.player[-1]
         cur_row = head.row
         cur_col = head.column
         cur_dir_x,cur_dir_y = self.player_curdire
